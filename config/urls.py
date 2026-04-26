cc61662444e0c36f5efa77400744d7e711fb64d6
C.Indhupriya <117663766+Dhivyaindhu@users.noreply.github.com>
2026-04-26 20:24:52 +0530

    Replace TemplateView with custom serve_frontend function

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+
+def serve_frontend(request):
+    with open(os.path.join(settings.BASE_DIR, 'templates', 'index.html'), 'r') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='text/html')
 
 urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/kyc/', include('kyc.urls')),
     path('api/notifications/', include('notifications.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', serve_frontend),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
